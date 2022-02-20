8694034334f9320e3bd57a89be2ae6fa27d1bb55
Jatin-ddev <99393408+Jatin-ddev@users.noreply.github.com>
2022-02-20 23:01:32 +0530

    Create Printing 3 statements.C

---
+++ Printing 3 statements.C
@@ -0,0 +1,16 @@
+#include <stdio.h>                           // For printf() & Scanf()
+
+int main()                                 // starting point of program Execution
+{
+
+ printf(" Hi \n ");                    // for printing 1 st statement
+ printf("Welcome To C");            // for printing 2 nd statement
+ printf(" \n hi there ");        // For Printing the 3 statement 
+ 
+ return 0;            /*         Returns the value to the operating
+						                   	system to
+								        	indicate that the program terminated
+								        	normally   */
+    
+    
+}
