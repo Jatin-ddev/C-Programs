f4018a510929465cba63f0eb6011cbc47154bfe5
Jatin-ddev <99393408+Jatin-ddev@users.noreply.github.com>
2022-02-20 23:04:17 +0530

    Create  printinganintegerFromuser.C

---
+++ printinganintegerFromuser.C
@@ -0,0 +1,19 @@
+#include <stdio.h>                            // For Printf() & Scanf()
+
+int main()                                        // Starting Point of the program
+{
+  int a;                                           // Variable Declaration 
+
+
+  printf("Enter a integer number\n");              // Request for input 
+  scanf("%d", &a);                                   // Input from user 
+
+  printf("You have Entered number %d\n", a);         // Printing on the output screen .
+
+  return 0;    // End Status For Indicating that the program terminated successfully
+}
+
+
+
+//   %d , %i can be used to denote integer values 
+
