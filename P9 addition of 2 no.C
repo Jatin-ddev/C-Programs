b787b2c323a187dd230dca83a9e41c8c812f7d2b
Jatin Rawat <99393408+Jatin-ddev@users.noreply.github.com>
2022-02-21 21:50:28 +0530

    Updated And Added Comments

---
+++ P9 addition of 2 no.C
@@ -0,0 +1,21 @@
+//Program 9 addition of 2 no.
+
+#include<stdio.h>           // For Printf() & Scanf()
+
+int main(){                 // starting Point Of the Program
+
+
+int num1, num2 , sum;               // Variable Declaration
+
+printf(" Enter Two Numbers: ");      // Request For Input
+scanf("%d%d",&num1,&num2);              // Input From User
+
+sum=num1+num2;                          // Addition formula
+
+printf(" The Addition Is %d",sum);      // Printing The Result On the Output Console
+
+return 0;               // End status For Indicating That The Program Terminated Successfully
+
+}
+
+
