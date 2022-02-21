44264db06a7daf25f5b97c192779cb87e0012352
Jatin Rawat <99393408+Jatin-ddev@users.noreply.github.com>
2022-02-21 21:51:28 +0530

    Updated and added Comments

---
+++ P9.1 Division Mystery.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main()
+{
+
+printf(“\n Division operation Result”);
+printf(“\n Two Integers (5 & 2) : %d”,5/2);
+printf(“\n One int & one float (5.5 & 2) : %g”,5.5/2);
+printf(“\n Two Integers (5 & 2) : %g”,(float)5/2);
+}
+
