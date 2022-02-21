1614ad238178d493d34a5fd61afdaac97dcd815c
Jatin Rawat <99393408+Jatin-ddev@users.noreply.github.com>
2022-02-21 21:46:54 +0530

    Happy programming

---
+++ P5 Print Float Values From user.C
@@ -0,0 +1,19 @@
+//Program 5 Print Float Values From user
+
+#include<stdio.h>                                               //  For Printf() & Scanf()
+
+int main(){                                                     // Starting Point Of The Program 
+
+
+float a;                                                        // Variable Declaration 
+
+printf("Enter The Value : \n ");                                    // Request for Input 
+scanf("%f",&a);                                                 // Input From User 
+
+printf(" The Entered Value Is:\n %f ", a);                     // For Printing On The Output 
+
+return 0;                                                       // End status For Indicating That The Program Terminated Successfully!!!
+}
+
+
+// %f , %e Denotes For Float Values!
