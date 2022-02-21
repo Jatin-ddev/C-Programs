25d5e68812205cc38b0cd278ab0a9a054838109a
Jatin Rawat <99393408+Jatin-ddev@users.noreply.github.com>
2022-02-21 21:49:13 +0530

    Updated! With Comments

---
+++ P8 Simple Divison.C
@@ -0,0 +1,17 @@
+
+//Program 8 Simple Divison
+
+#include<stdio.h>		// for Printf() & Scanf()
+
+int main(){			// starting Point Of The Program
+
+
+float num1,num2;		// Variable Declaration
+
+printf("Enter Two Numbers for Division ");			// Request for Input
+scanf("%f%f",&num1,&num2);					// Input From User
+
+printf("\n Div=%f",num1/num2);			// for Printing output on the console
+
+return 0;                       // For Indicating That The Program Terminated Successfully
+}
