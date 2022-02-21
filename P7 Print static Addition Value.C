bf9ec2f49946b0f14699cd539c0cbf35ccab168d
Jatin Rawat <99393408+Jatin-ddev@users.noreply.github.com>
2022-02-21 21:48:26 +0530

    Updated!

---
+++ P7 Print static Addition Value.C
@@ -0,0 +1,20 @@
+
+//Program 7 Print static Addition Value
+
+#include<stdio.h>                                           // For Printf() & Scanf()
+
+int main(){                                                 // Starting Point Of The Program 
+    
+    
+    int a=15, b=15 , c;                             // Variable Declaration + Intialisation 
+    c=a+b;                                                  // Addition Of Numbers 
+    
+    printf(" The Addition Is :%i", a+b);                    // For Priniting the Output 
+    
+    return 0;                               // End Status For Indicating That The Program Terminated successfully 
+}
+
+
+
+
+
