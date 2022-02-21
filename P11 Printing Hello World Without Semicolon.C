9e51c26738acb28fa89c7578ea1414e35e807e02
Jatin Rawat <99393408+Jatin-ddev@users.noreply.github.com>
2022-02-21 21:55:46 +0530

    Updated And Added Comments

---
+++ P11 Printing Hello World Without Semicolon.C
@@ -0,0 +1,52 @@
+//Program 11 Printing Hello World Without Semicolon
+
+
+
+// By Using If Statement 
+
+
+#include<stdio.h>
+
+int main(){
+
+ if(printf("hello world")){
+    // Empty 
+ }
+
+
+return 0;
+
+}
+
+
+// Method 2 By Using Switch Statement
+
+#include<stdio.h>
+
+int main(){
+
+ switch(printf("Hello World")){
+     // Empty Statements
+ }
+
+
+return 0;
+
+}
+
+
+// Method 03: By Using While Loop
+
+#include<stdio.h>
+
+int main(){
+
+while(!printf("hello world")){
+    // Empty Statements
+    //If This is not written ! then it will run to Infinite!
+}
+
+
+return 0;
+
+}
