716cea89aecedc62843edd2c7f74d909cb95964b
Jatin Rawat <99393408+Jatin-ddev@users.noreply.github.com>
2022-02-21 22:13:38 +0530

    Updated and added comments

---
+++ P15 Subtraction without minus operator.C
@@ -0,0 +1,18 @@
+#include<stdio.h>			
+
+int main(){				
+
+    int num1,num2;
+    int sub;
+
+    printf("Enter any two integers: ");
+    scanf("%d%d",&a,&b);
+
+    sub = a + ~b + 1;
+
+    printf("Difference of two integers: %d",sum);
+
+    return 0;
+}
+
+
