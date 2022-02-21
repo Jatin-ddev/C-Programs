6658b53453ac844e3cba1d6dc4466c05c4564486
Jatin Rawat <99393408+Jatin-ddev@users.noreply.github.com>
2022-02-21 21:47:39 +0530

    Updated!

---
+++ P6 Print Char Values From user.C
@@ -0,0 +1,19 @@
+
+
+//Program 6 Print Char Values From user
+
+#include<stdio.h>                       // For Printf & Scanf()
+
+int main(){                                 // Starting Point Of The Program 
+    
+    char a;                                     // Variable Declaration 
+    
+    printf(" Enter the Value : \n ");           // Request for Input 
+    scanf(" %c", &a);                           // Input From User 
+    
+    printf("The Entered Value Is : %c", a);                     //Printing On The Output Screen 
+    
+    return 0;                                           // End status Indicating That the Program Terminated Successfully 
+}
+
+
