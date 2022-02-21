a6aa7dd3f4676bbb9ba63e463d1204243fc8a766
Jatin Rawat <99393408+Jatin-ddev@users.noreply.github.com>
2022-02-21 22:19:31 +0530

    Updated

---
+++ P19 Area Of Square.C
@@ -0,0 +1,21 @@
+// Area Of Square : side * side
+
+
+#include <stdio.h>
+
+
+int main()
+{
+
+  int side;
+  long int Ar;
+
+  printf("Enter The Area Of Sqaure : ");
+  scanf("%d",&side);
+
+  Ar=side*side;
+
+  printf("\n The Area Of Sqaure is %ld",Ar);
+
+  return 0;
+}
