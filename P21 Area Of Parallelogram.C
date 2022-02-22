893b52e3cf2f1bee97b1fd9c4c6b576294e34323
Jatin Rawat <99393408+Jatin-ddev@users.noreply.github.com>
2022-02-22 15:00:07 +0530

    Updated

---
+++ P21 Area Of Parallelogram.C
@@ -0,0 +1,25 @@
+
+
+
+// Area Of Parallelogram = base*height
+
+#include<stdio.h>
+
+int main(){
+
+int bs,hg;
+long int Ar;
+
+printf("Enter The Base Of Parallelogram : ");
+scanf("%d",&bs);
+
+printf("Enter The Height : ");
+scanf("%d",&hg);
+
+Ar=bs*hg;
+
+printf("\n The Area Of Parallelogram is %ld",Ar);
+
+
+return 0;
+}
